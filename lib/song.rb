c8da0820c54a4bf1e7821d58a9fafadf39d33449
Brian Tobal <brian.tobal@gmail.com>
2019-04-11 10:59:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,54 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  #def initialize(name)
+  #  @name = name 
+  #end 
+
+  def self.create
+    new_song = self.new 
+    new_song.save
+    new_song
+  end 
+  
+  def self.new_by_name(song_name)
+    new_song = self.new 
+    new_song.name = song_name 
+    new_song
+  end 
+
+  def self.create_by_name(song_name)
+    new_song = self.new 
+    new_song.name = song_name 
+    new_song.save
+    new_song
+  end 
+
+  def self.find_by_name(name)
+    @@all.detect do |song|
+      if song.name == name 
+        return song 
+      else 
+        false 
+      end 
+    end
+  end 
+  
+ # def self.find_or_create_by_name(name)
+  #  if self.find_by_name(name)
+  #  else                          
+  #    Song.create_by_name(name)
+  #  end 
+  #end 
+    
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+
+  self.alphabetical 
+  
+  end 
+
   def self.all
     @@all
   end
